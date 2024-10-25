0324973f68a11713a913e5b14c679873c0dcd202
Mely <meliusanorahariyanti@gmail.com>
2024-10-25 12:25:19 +0700

    dashboard w/o aside done

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,14 @@
+<!--begin::Footer-->
+<div class="py-4 footer d-flex flex-lg-column" id="kt_footer">
+    <!--begin::Container-->
+    <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+        <!--begin::Copyright-->
+        <div class="order-2 text-dark order-md-1">
+            <span class="text-muted fw-bold me-1">2024 ©</span>
+            <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">MNH</a>
+        </div>
+        <!--end::Copyright-->
+    </div>
+    <!--end::Container-->
+</div>
+<!--end::Footer-->
